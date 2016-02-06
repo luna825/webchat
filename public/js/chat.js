af456b271efcf0b58d20108378b726236f928474
luna825 <luna825@qq.com>
2016-02-06 10:38:10 +0800

    webchat socket.io

---
+++ public/js/chat.js
@@ -0,0 +1,43 @@
+var Chat = function (socket) {
+    this.socket = socket;
+}
+
+Chat.prototype.sendMessage = function (room, text){
+  var message = {
+    room: room,
+    text:text
+  }
+  this.socket.emit('message',message)
+  console.log(message.room)
+}
+
+Chat.prototype.changeRoom = function(room) {
+  this.socket.emit('join',{
+    newRoom : room
+  })
+};
+
+Chat.prototype.processCommand = function(command) {
+  var words = command.split(' ');
+  var command = words[0]
+                  .substring(1,words[0].length)
+                  .toLowerCase();
+  var message = null;
+  switch(command){
+    case 'join':
+      words.shift();
+      var room = words.join(' ');
+      this.changeRoom(room);
+      break;
+    case 'nick':
+      words.shift();
+      var name = words.join(' ');
+      this.socket.emit('nameAttemp',name);
+      break;
+    default:
+      message = 'Unrecognized command';
+      break;
+  }
+  return message;
+
+};
